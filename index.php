78868625242d55ef22a8710539036a10973dde69
Evrard Caron <evrard.caron@etu.unicaen.fr>
2015-12-07 04:40:49 +0100

    Gestion des saisons en cours. Corrections css pour la frise. Ajout de la photo dans le aside

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 require_once 'handler/twig.php';
 
-echo $twig->render('activites.html.twig');
+$dateCourante = new DateTime();
+$annee = date('Y');
+$datePrintemps = new DateTime($annee.'-02-15');
+$dateEte = new DateTime($annee.'-06-01');
+$dateHiver = new DateTime($annee.'-11-15');
+
+if ($dateCourante >= $datePrintemps && $dateCourante < $dateEte)
+	$saison = 'printemps' ;
+elseif ($dateCourante >= $dateEte && $dateCourante < $dateHiver)
+	$saison = 'ete';
+else
+	$saison = 'hiver';
+
+echo $twig->render('index.html.twig', [
+	'saison' => 'ete'
+]);
